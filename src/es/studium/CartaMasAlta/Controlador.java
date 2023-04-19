829a2d5ea08da39300d6fda252a2af4ecba692ef
mjmartinezstudium <mjmartinezstudium@gmail.com>
2023-04-19 09:47:25 +0200

    Images

--- src/es/studium/CartaMasAlta/Controlador.java
+++ src/es/studium/CartaMasAlta/Controlador.java
@@ -1,6 +1,9 @@
 package es.studium.CartaMasAlta;
 
-public class Controlador 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class Controlador implements WindowListener 
 {
 	Modelo modelo;
 	MenuPrincipal menuPrincipal;
@@ -9,5 +12,56 @@ public class Controlador
 	{
 		this.modelo = m;
 		this.menuPrincipal = mp;
+		// Añadir Listeners
+		this.menuPrincipal.addWindowListener(this);
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e)
+	{
+		System.exit(0);
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e)
+	{
+		// TODO Auto-generated method stub
+		
 	}
 }
--- src/es/studium/CartaMasAlta/MenuPrincipal.java
+++ src/es/studium/CartaMasAlta/MenuPrincipal.java
@@ -1,9 +1,37 @@
 package es.studium.CartaMasAlta;
 
-public class MenuPrincipal
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+public class MenuPrincipal extends Frame
 {
+	private static final long serialVersionUID = 1L;
+
+	// Objetos que contienen imágenes
+	Image nuevaPartida, ayuda, ranking, salir;
+	// Objeto para manipulación de imágenes
+	Toolkit herramienta;
+
 	public MenuPrincipal()
 	{
-		
+		setLayout(null);
+		setTitle("Carta Más Alta");
+		setSize(600,400);
+		setBackground(Color.black);
+		// Activamos la herramienta
+		herramienta = getToolkit();
+		// Cargar la imagen
+		nuevaPartida = herramienta.getImage("newGame.png");
+		setResizable(false);
+		setVisible(true);
+	}
+	
+	public void paint(Graphics g)
+	{
+		// Dibujar la imagen
+		g.drawImage(nuevaPartida,220,25,this);
 	}
 }
